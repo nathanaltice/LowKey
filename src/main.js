efb8d6b9a48cb967c8355c59ef9b083bfed73b70
Nathan Altice <naltice@ucsc.edu>
2020-04-22 14:05:22 -0700

    slight tweaks, added comments

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ class AnyKey extends Phaser.Scene {
     }
 
     preload() {
+        // load our assets
         this.load.path = "assets/";
         this.load.image('balloon', 'balloon.png');
         this.load.image('skull', 'skull.png');
@@ -14,9 +15,9 @@ class AnyKey extends Phaser.Scene {
         // print message
         this.add.text(centerX, centerY, 'Press any key to continue').setOrigin(0.5);
 
-        // monitor any key down
+        // monitor *any* key down...
         this.input.keyboard.on('keydown', () => {
-            this.scene.start("cursorsScene");   // switch scenes
+            this.scene.start("cursorsScene");   // ...and switch scenes
         }, this);
     }
 }
@@ -35,7 +36,7 @@ class Cursors extends Phaser.Scene {
         this.add.text(centerX, h-100, 'Cursors (Press arrows, shift, or space)').setOrigin(0.5);
         this.add.text(centerX, h-50, 'Press \'S\' to change Scene').setOrigin(0.5);
 
-        // define cursors and S key
+        // define cursors and S key (for Scene switching)
         cursors = this.input.keyboard.createCursorKeys(); console.log(cursors);
         swap = this.input.keyboard.addKey('S');
         swap.on('down', () => {
@@ -44,6 +45,7 @@ class Cursors extends Phaser.Scene {
     }
 
     update() {
+        // check for all cursors object keys
         if (cursors.up.isDown) {
             this.message.text = "UP";
         } else if (cursors.down.isDown) {
@@ -112,27 +114,28 @@ class KeyCombo extends Phaser.Scene {
         this.add.text(centerX, h-100, 'Type \'facade\' to maek many skel').setOrigin(0.5);
         //this.add.text(centerX, h-50, 'Press \'S\' to change Scene').setOrigin(0.5);
 
-        // define key combo and S key
+        // define two key combos
+        // you can type this one as slow as you want
         let facadeCombo = this.input.keyboard.createCombo('facade', {
             resetOnWrongKey: true,  // if they press the wrong key is the combo reset?
             maxKeyDelay: 0,       // max delay (ms) between each key press (0 = disabled)
             resetOnMatch: true,    // if matched before, does pressing first key of combo reset?
             deleteOnMatch: false     // if combo matches, will it delete itself?
         });
-
+        // for this one, you gotta type fast
         let fastCombo = this.input.keyboard.createCombo('sanic', {
             resetOnWrongKey: true,  // if they press the wrong key is the combo reset?
             maxKeyDelay: 200,       // max delay (ms) between each key press (0 = disabled)
             resetOnMatch: true,    // if matched before, does pressing first key of combo reset?
             deleteOnMatch: true     // if combo matches, will it delete itself?
         });
-
+        // watch for keycombomatches
         this.input.keyboard.on('keycombomatch', (combo, event) => {
             if (combo === facadeCombo) { 
                 this.add.sprite(0,0, 'skull').setRandomPosition();
             }
             if (combo === fastCombo) {
-                this.add.sprite(centerX, centerY, 'cursed').setDepth(-1);
+                this.add.sprite(centerX, centerY, 'cursed').setScale(0.5).setDepth(-1);
             }   
         });
 
