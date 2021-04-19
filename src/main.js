176eeb5e11d88f5ce50dd7141da6fe2f38af13e4
Nathan Altice <naltice@ucsc.edu>
2021-04-19 13:38:15 -0700

    minor variable tweaks

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Nathan Altice
 // Updated: 4/9/21
 // Low Key
-// Demonstrating some Phaser Keyboard events and objects
+// Demonstrating a few Phaser Keyboard events and objects
 
 class AnyKey extends Phaser.Scene {
     constructor() {
@@ -38,8 +38,8 @@ class Cursors extends Phaser.Scene {
 
         // print messages
         this.message = this.add.text(centerX, centerY, '').setOrigin(0.5);
-        this.add.text(centerX, h-100, 'Cursors (Press arrows, shift, or space)').setOrigin(0.5);
-        this.add.text(centerX, h-50, 'Press \'S\' to change Scene').setOrigin(0.5);
+        this.add.text(centerX, height-100, 'Cursors (Press arrows, shift, or space)').setOrigin(0.5);
+        this.add.text(centerX, height-50, 'Press \'S\' to change Scene').setOrigin(0.5);
 
         // define cursors and S key (for Scene switching)
         cursors = this.input.keyboard.createCursorKeys();
@@ -85,8 +85,8 @@ class JustDown extends Phaser.Scene {
 
         // print messages
         this.message = this.add.text(centerX, centerY, '').setOrigin(0.5);
-        this.add.text(centerX, h-100, 'Tap UP ARROW to make balloon go up').setOrigin(0.5);
-        this.add.text(centerX, h-50, 'Press \'S\' to change Scene').setOrigin(0.5);
+        this.add.text(centerX, height-100, 'Tap UP ARROW to make balloon go up').setOrigin(0.5);
+        this.add.text(centerX, height-50, 'Press \'S\' to change Scene').setOrigin(0.5);
 
         // define cursors and S key
         cursors = this.input.keyboard.createCursorKeys();
@@ -119,22 +119,22 @@ class KeyCombo extends Phaser.Scene {
 
         // print messages
         this.message = this.add.text(centerX, centerY, '').setOrigin(0.5);
-        this.add.text(centerX, h-100, 'Type \'facade\' to maek many skel').setOrigin(0.5);
-        //this.add.text(centerX, h-50, 'Press \'S\' to change Scene').setOrigin(0.5);
+        this.add.text(centerX, height-100, 'Type \'facade\' to maek many skel').setOrigin(0.5);
+        //this.add.text(centerX, height-50, 'Press \'S\' to change Scene').setOrigin(0.5);
 
         // define two key combos
         // you can type this one as slow as you want
         let facadeCombo = this.input.keyboard.createCombo('facade', {
             resetOnWrongKey: true,  // if they press the wrong key is the combo reset?
-            maxKeyDelay: 0,       // max delay (ms) between each key press (0 = disabled)
-            resetOnMatch: true,    // if matched before, does pressing first key of combo reset?
-            deleteOnMatch: false     // if combo matches, will it delete itself?
+            maxKeyDelay: 0,         // max delay (ms) between each key press (0 = disabled)
+            resetOnMatch: true,     // if matched before, does pressing first key of combo reset?
+            deleteOnMatch: false    // if combo matches, will it delete itself?
         });
         // for this one, you gotta type fast
         let fastCombo = this.input.keyboard.createCombo('sanic', {
             resetOnWrongKey: true,  // if they press the wrong key is the combo reset?
             maxKeyDelay: 200,       // max delay (ms) between each key press (0 = disabled)
-            resetOnMatch: true,    // if matched before, does pressing first key of combo reset?
+            resetOnMatch: true,     // if matched before, does pressing first key of combo reset?
             deleteOnMatch: true     // if combo matches, will it delete itself?
         });
         // watch for keycombomatches
@@ -160,8 +160,7 @@ let config = {
 let game = new Phaser.Game(config);
 
 // some globals
-let w = game.config.width;
-let h = game.config.height;
-let centerX = game.config.width / 2;
-let centerY = game.config.height / 2;
+const { width, height } = game.config;
+let centerX = width / 2;
+let centerY = height / 2;
 let cursors = null;
